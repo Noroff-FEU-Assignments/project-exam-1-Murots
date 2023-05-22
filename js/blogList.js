cc55b038645ba4336f1e084e1cf5ca7d5d8821a1
Aleksander Storum <alesto95654@stud.noroff.no>
2023-05-23 00:38:25 +0200

    Added "load more blogs" functionality to blogList.js and improved usability and interaction

--- js/blogList.js
+++ js/blogList.js
@@ -1,31 +1,33 @@
 const blogListContainer = document.querySelector(".blogList-container");
+const loadMoreButton = document.querySelector("#loadMore-button");
 
 const apiBase = "https://bringthebugsback.murots.no/";
 const postsBase = "wp-json/wp/v2/posts";
-const tenLatestPostsBase = "?per_page=10&order=desc";
+let postValue = 1;
+let currentPage = `?page=${postValue}`;
 
-const fullLatestPostsURL = apiBase + postsBase + tenLatestPostsBase;
+const pageURL = apiBase + postsBase + currentPage;
 
-async function getLatestPosts() {
+async function getPosts() {
     try {
-        const response = await fetch(fullLatestPostsURL);
-        const latestPosts = await response.json();
-        console.log(latestPosts);
-        return latestPosts;
+        const response = await fetch(pageURL);
+        const posts = await response.json();
+        console.log(posts);
+        return posts;
     } catch (error) {
         console.error(error);
         blogListContainer.innerHTML = errorMessage("Could not fetch data. Please try again later.");
     }
 }
 
-async function createPostListHTML(latestPost) {
+async function createPostListHTML(post) {
     try {
-        const featuredImageId = latestPost.featured_media;
+        const featuredImageId = post.featured_media;
         const featuredImageURL = apiBase + "wp-json/wp/v2/media/" + featuredImageId;
         const response = await fetch(featuredImageURL);
         const featuredImage = await response.json();
 
-        const blogId = latestPost.id;
+        const blogId = post.id;
         const blogListImage = featuredImage.source_url;
 
         const blogContainer = document.createElement("a");
@@ -41,7 +43,7 @@ async function createPostListHTML(latestPost) {
 
         const blogTitle = document.createElement("h1");
         blogTitle.className = "blogContainer-title";
-        blogTitle.innerText = latestPost.title.rendered;
+        blogTitle.innerText = post.title.rendered;
         TextContent.append(blogTitle);
 
         const readMore = document.createElement("h4");
@@ -54,18 +56,47 @@ async function createPostListHTML(latestPost) {
     }
 }
 
-function createLatestPostsHTML(latestPosts) {
-    for (let i = 0; i < latestPosts.length; i++) {
-        const latestPost = latestPosts[i];
-        console.log(latestPost);
-        createPostListHTML(latestPost);
+function createPostsHTML(posts) {
+    for (let i = 0; i < posts.length; i++) {
+        const post = posts[i];
+        console.log(post);
+        createPostListHTML(post);
     }
 }
-  
+
+async function loadMorePosts() {
+    try {
+        postValue++;
+        currentPage = `?page=${postValue}`;
+        const nextPageURL = apiBase + postsBase + currentPage;
+
+        const response = await fetch(nextPageURL);
+        const posts = await response.json();
+        console.log(posts);
+        createPostsHTML(posts);
+
+        if (posts.length < 10) {
+            loadMoreButton.disabled = true;
+            loadMoreButton.style.backgroundColor = "#717c5b"; 
+        }
+    } catch (error) {
+        console.error(error);
+        blogListContainer.innerHTML = errorMessage("Could not fetch data. Please try again later.");
+    }
+}
+
+loadMoreButton.addEventListener("click", loadMorePosts);
+
+
 async function main() {
     try {
-        const latestPosts = await getLatestPosts();
-        createLatestPostsHTML(latestPosts);
+        const posts = await getPosts();
+        createPostsHTML(posts);
+
+        if (posts.length < 10) {
+            loadMoreButton.disabled = true;
+        }
+
         const loaderDiv = document.querySelector(".loader");
         loaderDiv.remove();
     } catch (error) {
@@ -73,5 +104,5 @@ async function main() {
         blogListContainer.innerHTML = errorMessage("Could not fetch data. Please try again later.");
     }
 }
-  
+
 main();
